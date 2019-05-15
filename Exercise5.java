1aeee2deb10467bf77950f1157b19cfe86b1b008
Gbemisola N <gbemikp@gmail.com>
2019-05-15 07:12:56 +0100

    displaying of prime number

---
+++ Exercise5.java
@@ -0,0 +1,27 @@
+// program to accept prime number from user
+import java.util.Scanner;
+public class Exercise5{
+    public static void main(String[] args){
+         Scanner s = new Scanner(System.in);
+        System.out.println("enter a number");
+        int n=s.nextInt();
+        if(isPrime(n)){
+            System.out.println(n+  "is a prime number");
+        }
+        else{
+            System.out.println(n+  "is not a prime number");
+
+        }
+    }
+public static boolean isPrime(int n){
+    if(n<=1){
+        return false;
+    }
+    for(int i = 2;i< Math.sqrt(n);i++){
+        if(n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
+}
